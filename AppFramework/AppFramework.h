4872e0ec2b5a8bac5c7621605c330d9ec709e2dd
fujl <jialin.fu@longmaster.com.cn>
2017-12-15 14:26:55 +0800

    Initial Commit

---
+++ AppFramework/AppFramework.h
@@ -0,0 +1,19 @@
+//
+//  AppFramework.h
+//  AppFramework
+//
+//  Created by fujl-mac on 2017/12/15.
+//  Copyright © 2017年 fujl-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppFramework.
+FOUNDATION_EXPORT double AppFrameworkVersionNumber;
+
+//! Project version string for AppFramework.
+FOUNDATION_EXPORT const unsigned char AppFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppFramework/PublicHeader.h>
+
+
